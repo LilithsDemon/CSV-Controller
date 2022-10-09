9aa9ef4c0110942e929425922c81ed9400933410
MiniMECHlg <lucy.gardener2004@gmail.com>
2022-10-09 19:12:07 +0100

    Added basic reading of files ready for editing

--- csvcontroller.cpp
+++ csvcontroller.cpp
@@ -1,14 +1,74 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <string.h>
 #include <vector>
 
-int main()
+std::vector<std::string> splitCommand(std::string command, std::string seperator)
 {
     std::vector<std::string> commands;
+    int last_stop = 0;
+    std::string current_text = "";
+    int current_command = 0;
+
+    for (int i = 0; i < (command.length() - seperator.length()); i++)
+    {
+        current_text = command.substr(i, seperator.length());
+        if(current_text == seperator)
+        {
+            commands.push_back(command.substr(last_stop, i - last_stop));
+            current_command++;
+            last_stop = i+seperator.length();
+        }
+    }
+    commands.push_back(command.substr(last_stop));
+    if (commands.size() == 0)
+    {
+        commands.push_back(command);
+    }
+    
+    return commands;
+}
+
+void writeToFile(std::string text, std::string file_name)
+{
+    std::ofstream file(file_name);
+    file << text;
+    file.close();
+}
+
+std::vector<std::vector<std::string>> readFile(std::string file_name)
+{
+    std::vector<std::string> file_text;
+    std::ifstream file(file_name);
+    std::string text = "";
+    std::string seperator = ",";
+    std::vector<std::vector<std::string>> split_text;
+
+    while (getline(file, text))
+    {
+        file_text.push_back(text);
+    }
+
+    file.close();
+
+    for(int i = 0; i < file_text.size(); i++)
+    {
+        split_text.push_back(splitCommand(file_text[i], seperator));
+    }
+
+    return split_text;
+}
+
+int main()
+{
+    std::string file_name = "";
     std::string seperator = " -";
-    std::string file = "";
     std::string user_input = "";
+    std::vector<std::vector<std::string>> file_data;
+    bool last_updated = false;
+    int col = 0;
+    int row = 0;
 
     bool run = true;
 
@@ -19,47 +79,74 @@ int main()
         std::cout << ">>: ";
         std::getline(std::cin, user_input);
 
-        if (user_input == "q" || user_input == "quit")
-        {
-            exit(0);
-        }
+        std::vector<std::string> commands = splitCommand(user_input, seperator);
 
-        int last_stop = 0;
-        std::string current_text = "";
-                break;
-
-        for (int i = 0; i < (user_input.length() - seperator.length()); i++)
-        {
-            current_text = user_input.substr(i, seperator.length());
-            if(current_text == seperator)
-            {
-                commands.push_back(user_input.substr(last_stop, i - last_stop));
-                current_command++;
-                last_stop = i+seperator.length();
-            }
-            commands.push_back(user_input.substr(last_stop, i - last_stop + seperator.length() + 1));
-        }
-
-        std::string file_name = "";
+        
+        std::string val = "";
 
         for (int i = 0; i < commands.size(); i++)
         {
             if (commands[i].substr(0, 4) == "file")
             {
-                file_name = commands[i].substr(3);
+                file_name = commands[i].substr(5);
+                file_data = readFile(file_name);
             }
-        }
-                
-        std::cout << file_name;
-
-        for (int i = 0; i < 16; i++)
-        {
-            if(commands[i] == "")
+            if (commands[i].substr(0,4) == "read")
             {
-                break;
+                if(file_name == "")
+                {
+                    std::cout << "\nNo file selected please select a file and retry\n";
+                    break;
+                }
+                else
+                {
+                    for(int i = 0; i < file_data.size(); i++)
+                    {
+                        for(int j = 0; j < file_data[i].size(); j++)
+                        {
+                            std::cout << file_data[i][j] << "\t";
+                        }
+                        std::cout << "\n";
+                    }
+                }
             }
-
-            std::cout << commands[i] << "\n";
+            if (commands[i].substr(0, 6) == "append")
+            {
+                if(file_name == "")
+                {
+                    std::cout << "\nNo file selected please select a file and retry\n";
+                    break;
+                }
+                if (last_updated == false && col != 0)
+                {
+                    //check column position
+                    //if the next value does not exist or is a ,
+                    //Then put the new value
+                }
+                if (last_updated == true)
+                {
+                    //if last value is, or does not exist
+                    //Put the new value
+                }
+            }
+            if (commands[i].substr(0,3) == "col")
+            {
+                col = stoi(commands[i].substr(4));
+                last_updated = false;
+            }
+            if (commands[i].substr(0,3) == "row")
+            {
+                row = stoi(commands[i].substr(4));
+                last_updated = true;
+            }
+            if (commands[i].substr(0, 3) == "val")
+            {
+                val = commands[i].substr(4);
+            }
+            if (commands[i] == "q" || commands[i] == "quit")
+            {
+                exit(0);
+            }
         }
     }
     std::cout << "\n";
